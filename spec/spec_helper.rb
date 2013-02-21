f0943a9c24969ca54132835a7e554ea99e2ca07c
Mark Rebec <mark@markrebec.com>
2013-02-21 14:29:16 -0800

    bit of cleanup to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
-ENV["RAILS_ENV"] ||= 'test'
 require 'active_support'
 require 'active_record'
 require 'action_controller'
@@ -8,13 +7,10 @@ require 'database_cleaner'
 
 Dir[File.join(File.dirname(__FILE__), '..', "spec/support/**/*.rb")].each { |f| require f }
 
-ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
-capture_stdout { load "spec/db/schema.rb" }
-
 RSpec.configure do |config|
-  #config.use_transactional_fixtures = false # Using DatabaseCleaner transactions instead
-
   config.before(:suite) do
+    ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+    capture_stdout { load "spec/db/schema.rb" }
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with :truncation
   end
@@ -26,9 +22,4 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-  
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  #config.infer_base_class_for_anonymous_controllers = true
 end
