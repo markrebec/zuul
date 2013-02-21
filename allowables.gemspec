afd1909b03423a71ff2c89f98acf9398f16d7226
Mark Rebec <mark@markrebec.com>
2013-02-21 15:55:37 -0800

    removing database_cleaner in favor of a simple around filter to roll everything back after each example

--- allowables.gemspec
+++ allowables.gemspec
@@ -19,5 +19,4 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency "sqlite3"
   s.add_development_dependency "rspec"
-  s.add_development_dependency "database_cleaner"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ require 'active_record'
 require 'action_controller'
 require 'allowables'
 require 'rspec'
-require 'database_cleaner'
 
 Dir[File.join(File.dirname(__FILE__), '..', "spec/support/**/*.rb")].each { |f| require f }
 
@@ -11,15 +10,12 @@ RSpec.configure do |config|
   config.before(:suite) do
     ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
     capture_stdout { load "spec/db/schema.rb" }
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with :truncation
   end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
+  config.around(:each) do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
   end
 end
