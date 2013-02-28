8af42b92d68bb24ec77fb6c7441f992d1cc86bb4
Mark Rebec <mark@markrebec.com>
2013-02-27 17:29:12 -0800

    removing :level from permissions - not sure how that was passing, guess the test models don't have attr_accessible/protected

--- spec/allowables/active_record/subject_spec.rb
+++ spec/allowables/active_record/subject_spec.rb
@@ -618,19 +618,19 @@ describe "Allowables::ActiveRecord::Subject" do
       end
 
       it "should accept an optional context" do
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         expect { @user.assign_permission(:edit, nil) }.to_not raise_exception
       end
 
       it "should use nil context when none is provided" do
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         permission_user = @user.assign_permission(:edit)
         permission_user.id.should_not be_nil
         permission_user.context_type.should be_nil
       end
 
       it "should use the context when one is provided" do
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         context = Context.create(:name => "Test Context")
         
         class_permission_user = @user.assign_permission(permission, Context)
@@ -646,9 +646,9 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should use target_permission to lookup the closest contextual match when a permission slug is provided" do
         context = Context.create(:name => "Test Context")
-        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
-        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context')
-        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context', :context_id => context.id)
+        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit')
+        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context')
+        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context', :context_id => context.id)
         
         nil_permission_user = @user.assign_permission(:edit, nil)
         nil_permission_user.id.should_not be_nil
@@ -671,9 +671,9 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should use the permission object when one is provided" do
         context = Context.create(:name => "Test Context")
-        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
-        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context')
-        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context', :context_id => context.id)
+        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit')
+        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context')
+        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context', :context_id => context.id)
         
         class_permission_user = @user.assign_permission(nil_permission, Context)
         class_permission_user.id.should_not be_nil
@@ -695,8 +695,8 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should fail and return false if the provided permission cannot be used within the provided context" do
         context = Context.create(:name => "Test Context")
-        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context')
-        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context', :context_id => context.id)
+        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context')
+        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context', :context_id => context.id)
 
         @user.assign_permission(class_permission, nil).should be_false
         @user.assign_permission(inst_permission, nil).should be_false
@@ -704,13 +704,13 @@ describe "Allowables::ActiveRecord::Subject" do
       end
 
       it "should create the permission_subjects record to link the subject to the provided permission" do
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         @user.assign_permission(permission)
         PermissionUser.where(:user_id => @user.id, :permission_id => permission.id).count.should == 1
       end
 
       it "should fail and return false if the provided permission is already assigned in the provided context" do
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         context = Context.create(:name => "Test Context")
         @user.assign_permission(permission)
         @user.assign_permission(permission).should be_false
@@ -731,12 +731,12 @@ describe "Allowables::ActiveRecord::Subject" do
       end
 
       it "should accept an optional context" do
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         expect { @user.unassign_permission(:edit, nil) }.to_not raise_exception
       end
 
       it "should use nil context when none is provided" do
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         @user.assign_permission(:edit, nil)
         @user.assign_permission(:edit, Context)
 
@@ -749,9 +749,9 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should use target_permission to lookup the closest contextual match when a permission slug is provided" do
         context = Context.create(:name => "Test Context")
-        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
-        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context')
-        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context', :context_id => context.id)
+        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit')
+        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context')
+        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context', :context_id => context.id)
         @user.assign_permission(nil_permission, nil)
         @user.assign_permission(class_permission, Context)
         @user.assign_permission(nil_permission, Context)
@@ -766,9 +766,9 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should use the permission object when one is provided" do
         context = Context.create(:name => "Test Context")
-        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
-        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context')
-        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context', :context_id => context.id)
+        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit')
+        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context')
+        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context', :context_id => context.id)
         @user.assign_permission(nil_permission, nil)
         @user.assign_permission(class_permission, Context)
         @user.assign_permission(nil_permission, Context)
@@ -787,7 +787,7 @@ describe "Allowables::ActiveRecord::Subject" do
       end
 
       it "should remove the permission_subjects record that links the subject to the provided permission" do
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         @user.assign_permission(permission)
         PermissionUser.where(:user_id => @user.id, :permission_id => permission.id).count.should == 1
         @user.unassign_permission(permission)
@@ -796,9 +796,9 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should fail and return false if the provided permission is not assigned in the provided context" do
         context = Context.create(:name => "Test Context")
-        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
-        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context')
-        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context', :context_id => context.id)
+        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit')
+        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context')
+        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context', :context_id => context.id)
         @user.assign_permission(nil_permission, Context)
         @user.assign_permission(class_permission, context)
         
@@ -818,12 +818,12 @@ describe "Allowables::ActiveRecord::Subject" do
       end
 
       it "should accept an optional context" do
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         expect { @user.has_permission?(:edit, nil) }.to_not raise_exception
       end
 
       it "should use nil context when none is provided" do
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         @user.assign_permission(:edit, Context)
         @user.has_permission?(:edit).should be_false
         @user.assign_permission(:edit, nil)
@@ -832,9 +832,9 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should use target_permission to lookup the closest contextual match when a permission slug is provided" do
         context = Context.create(:name => "Test Context")
-        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
-        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context')
-        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context', :context_id => context.id)
+        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit')
+        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context')
+        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context', :context_id => context.id)
         @user.assign_permission(nil_permission, Context)
         @user.assign_permission(class_permission, context)
         
@@ -850,9 +850,9 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should use the permission object when one is provided" do
         context = Context.create(:name => "Test Context")
-        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
-        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context')
-        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context', :context_id => context.id)
+        nil_permission = Permission.create(:name => 'Edit', :slug => 'edit')
+        class_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context')
+        inst_permission = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context', :context_id => context.id)
         @user.assign_permission(nil_permission, Context)
         @user.assign_permission(class_permission, context)
         
@@ -875,7 +875,7 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should look up the context chain for the assigned permission" do
         context = Context.create(:name => "Test Context")
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         @user.assign_permission(permission, nil)
         @user.has_permission?(:edit, Context).should be_true
         @user.has_permission?(:edit, context).should be_true
@@ -886,7 +886,7 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should return false if the provided permission is not assigned to the subject within the context chain" do
         context = Context.create(:name => "Test Context")
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         @user.has_permission?(permission, context).should be_false
         @user.has_permission?(:edit, Context).should be_false
         @user.has_permission?(permission, nil).should be_false
@@ -913,7 +913,7 @@ describe "Allowables::ActiveRecord::Subject" do
       end
       
       it "should use nil context when none is provided" do
-        edit_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        edit_permission = Permission.create(:name => 'Edit', :slug => 'edit')
         @user.assign_permission(edit_permission, Context)
         @user.permissions_for.length.should == 0
         @user.assign_permission(edit_permission, nil)
@@ -922,16 +922,16 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should return an empty association array if no permissions are assigned to the subject within the provided context" do
         context = Context.create(:name => "Test Context")
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         @user.assign_permission(permission, context)
         @user.permissions_for(nil).length.should == 0
         @user.permissions_for(Context).length.should == 0
       end
 
       it "should return all permissions assigned to the subject within the provided context" do
-        nil_edit = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
-        class_edit = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context')
-        class_view = Permission.create(:name => 'View', :slug => 'view', :level => 80, :context_type => 'Context')
+        nil_edit = Permission.create(:name => 'Edit', :slug => 'edit')
+        class_edit = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context')
+        class_view = Permission.create(:name => 'View', :slug => 'view', :context_type => 'Context')
         @user.assign_permission(nil_edit, Context)
         @user.assign_permission(class_edit, Context)
         @user.assign_permission(class_view, Context)
@@ -949,7 +949,7 @@ describe "Allowables::ActiveRecord::Subject" do
       end
       
       it "should use nil context when none is provided" do
-        edit_permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        edit_permission = Permission.create(:name => 'Edit', :slug => 'edit')
         @user.assign_permission(edit_permission, Context)
         @user.permissions_for?.should be_false
         @user.assign_permission(edit_permission, nil)
@@ -958,16 +958,16 @@ describe "Allowables::ActiveRecord::Subject" do
 
       it "should return false if no permissions are assigned to the subject within the provided context" do
         context = Context.create(:name => "Test Context")
-        permission = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
+        permission = Permission.create(:name => 'Edit', :slug => 'edit')
         @user.assign_permission(permission, context)
         @user.permissions_for?(nil).should be_false
         @user.permissions_for?(Context).should be_false
       end
 
       it "should return true if any permissions are assigned to the subject within the provided context" do
-        nil_edit = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100)
-        class_edit = Permission.create(:name => 'Edit', :slug => 'edit', :level => 100, :context_type => 'Context')
-        class_view = Permission.create(:name => 'View', :slug => 'view', :level => 80, :context_type => 'Context')
+        nil_edit = Permission.create(:name => 'Edit', :slug => 'edit')
+        class_edit = Permission.create(:name => 'Edit', :slug => 'edit', :context_type => 'Context')
+        class_view = Permission.create(:name => 'View', :slug => 'view', :context_type => 'Context')
         @user.assign_permission(nil_edit, Context)
         @user.assign_permission(class_edit, Context)
         @user.assign_permission(class_view, Context)
