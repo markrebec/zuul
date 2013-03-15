962bed7d4bae7667ec8cf3dff8930f37415ea9dc
Mark Rebec <mark@markrebec.com>
2013-03-14 17:49:16 -0700

    generators for auth models/migrations

---
+++ lib/generators/allowables/role_generator.rb
@@ -0,0 +1,56 @@
+require 'rails/generators/active_record'
+require 'generators/allowables/orm_helpers'
+
+module ActiveRecord
+  module Allowables
+    module Generators
+      class RoleGenerator < ActiveRecord::Generators::Base
+        argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
+        namespace "allowables:role"
+
+        include ::Allowables::Generators::OrmHelpers
+        source_root File.expand_path("../templates", __FILE__)
+        
+        def copy_allowables_migration
+          if (behavior == :invoke && model_exists?) || (behavior == :revoke && migration_exists?(:role, table_name))
+            migration_template "role_existing.rb", "db/migrate/add_allowables_role_to_#{table_name}"
+          else
+            migration_template "role.rb", "db/migrate/allowables_role_create_#{table_name}"
+          end
+        end
+
+        def generate_model
+          invoke "active_record:model", [name], :migration => false unless model_exists? && behavior == :invoke
+        end
+
+        def inject_allowables_content
+          content = <<CONTENT
+  # Setup authorization for your role model
+  acts_as_authorization_role
+CONTENT
+
+          class_path = if namespaced?
+            class_name.to_s.split("::")
+          else
+            [class_name]
+          end
+
+          indent_depth = class_path.size - 1
+          content = content.split("\n").map { |line| "  " * indent_depth + line } .join("\n") << "\n"
+
+          inject_into_class(model_path, class_path.last, content) if model_exists?
+        end
+
+        def migration_data
+<<RUBY
+      ## Authorization role columns
+      t.string  :slug
+      t.integer :level
+      t.string  :context_type
+      t.integer :context_id
+RUBY
+        end
+      end
+    end
+  end
+end
